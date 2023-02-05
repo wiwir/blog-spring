16348e0bd9b152497a2a31148d48ccda7ff0e696
“William <wramirez.externo@coopeuch.cl>
2023-02-04 23:12:40 -0300

    se agrega cors v2

--- src/main/java/com/example/springblog/controller/BlogPostController.java
+++ src/main/java/com/example/springblog/controller/BlogPostController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/post/v1")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins= {"*"}, maxAge = 4800, allowCredentials = "false" )
 
 public class BlogPostController {
 
